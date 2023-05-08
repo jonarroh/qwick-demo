c606edb26b92aee847927facc26ed1c4b4328eda
jonarroh <jonarrodi99@gmail.com>
2023-05-08 08:58:38 -0600

    ✨ feat (db) Added prima to use mysql

--- src/routes/hola/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-
-export default component$(() => {
-	return (
-		<>
-			<h1>Hola page</h1>
-		</>
-	);
-});
---
+++ src/routes/posts/index.tsx
@@ -0,0 +1,49 @@
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
+import { server$ } from '@builder.io/qwik-city';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+interface Post {
+	id: string;
+	createdAt: Date;
+	content: string;
+	authorId: string;
+	autorName: string;
+}
+
+export default component$(() => {
+	const post = useSignal<Post[]>([]);
+
+	const connectToDB = server$(async () => {
+		const data = await prisma.post.findMany();
+		return data;
+	});
+
+	useTask$(async () => {
+		post.value = await connectToDB();
+	});
+
+	return (
+		<>
+			<h1>Hola page</h1>
+			<button
+				onClick$={() => {
+					connectToDB().then(data => {
+						post.value = data;
+					});
+				}}>
+				ver post
+			</button>
+
+			{post &&
+				post.value.map((item, index) => {
+					return (
+						<div key={index}>
+							<h2>{item.autorName}</h2>
+							<p>{item.content}</p>
+						</div>
+					);
+				})}
+		</>
+	);
+});
